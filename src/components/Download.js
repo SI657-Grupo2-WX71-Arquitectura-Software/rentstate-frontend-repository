0b3cc6055b4cda4c8acd02aedb5290606c0fbb42
nirvanaGarciaV <84489764+nirvagarcia@users.noreply.github.com>
2024-04-03 00:04:42 -0500

    adding search textfield

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,28 +1,40 @@
 import React from "react";
 import "../styles/GoRentstate.css";
+import { Search } from "@mui/icons-material";
+import Box from '@mui/material/Box';
+import SearchIcon from '@mui/icons-material/Search';
+import { TextField } from "@mui/material";
+
 
 
 const Download = () => {    
+
+
+
+
     return (       
         <div id="goRentstate"> 
 
             <div className="goRentstate-container">
-
+                    
                 <div className="i" >
+                   
+                       
+                        <TextField
+                            id="outlined-textarea"
+                          
+                            placeholder="Buscar Inmueble"
+                            multiline
+                        />
+                     <SearchIcon />       
 
-                    <p style={{fontWeight: '400', margin: '0'}}>Prueba la Web <span style={{color:'#225E7C',fontWeight: '500'}}>RentState</span></p>
-
-                    <div style={{display:'flex', justifyContent:'center', alignContent:'center', alignItems:'center'}}>
-                        <div className="home-button" onClick={() =>  window.open("https://renstate-aedf7.web.app/home", "_blank")}>
-                            <p>Ir a RentState</p>
-                        </div>
-                    </div>
-
+                   
+        
                 </div>
 
-                <div className="download-img" >
+                {/* <div className="download-img" >
                     <img  src="/assets/RentStateBanner.png" alt="WhatsApp" />
-                </div>   
+                </div>    */}
 
             
                         
