b73b945d9842e946b295cf05ffbac448a1b27f09
nirvanaGarciaV <84489764+nirvagarcia@users.noreply.github.com>
2024-04-03 09:56:57 -0500

    adding search bar with correct style

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,45 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/GoRentstate.css";
-import { Search } from "@mui/icons-material";
-import Box from '@mui/material/Box';
 import SearchIcon from '@mui/icons-material/Search';
 import { TextField } from "@mui/material";
 
-
-
 const Download = () => {    
-
-
-
+    const [searchValue, setSearchValue] = useState("");
+   
+    const handleTextFieldChange = (event) => {
+        setSearchValue(event.target.value);
+    };
+   
+    const handleSearchIconClick = () => {
+        console.log("El user buscó:", searchValue);
+        setSearchValue("");
+    };
 
     return (       
         <div id="goRentstate"> 
 
             <div className="goRentstate-container">
                     
-                <div className="i" >
-                   
-                       
-                        <TextField
-                            id="outlined-textarea"
-                          
-                            placeholder="Buscar Inmueble"
-                            multiline
-                        />
-                     <SearchIcon />       
-
-                   
-        
-                </div>
-
-                {/* <div className="download-img" >
-                    <img  src="/assets/RentStateBanner.png" alt="WhatsApp" />
-                </div>    */}
-
-            
-                        
+                <div className="search-system" 
+                    style={{
+                        display:'flex',                       
+                        alignItems:'center',
+                        gap:'0.5rem'
+                    }}
+                >                   
+                    <TextField
+                        id="outlined-textarea"                          
+                        placeholder="Buscar Inmueble"
+                        value={searchValue}
+                        onChange={handleTextFieldChange}
+                        sx={{
+                            width: '30rem',                                               
+                            '& .MuiOutlinedInput-root': {
+                                borderRadius: '200px',                              
+                                '& fieldset': {
+                                    borderColor: '#ececec !important',                                   
+                                },
+                            },
+                            '& .MuiInputBase-input': { fontSize: '0.8rem',  padding:'0.8rem 1.3rem' }                          
+                        }}
+                    />
+                    <SearchIcon 
+                        style={{
+                            height:'1.5rem',   
+                            color:'gray',   
+                            cursor:'pointer'                         
+                        }}
+                        onClick={handleSearchIconClick}
+                    />   
+                </div>      
             </div>
-
         </div>      
     );
 };
