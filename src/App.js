809e7da59e366cdeb0e3df1200c55473b12ebd19
nirvanaGarciaV <84489764+nirvagarcia@users.noreply.github.com>
2024-04-03 21:22:21 -0500

    adding react router dom

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import './App.css';
 import HomeRentState from './components/HomeRentState';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MyAccount from './components/MyAccount';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<Navbar/>				
-				<HomeRentState/>			
-				<Footer/>			
-			</header>
-		</div>
-	);
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <Navbar/>				
+                    <Routes>
+                        <Route path="/MiCuenta" element={<MyAccount />} />
+                        <Route path="/" element={<HomeRentState />} />
+                    </Routes>
+                    <Footer/>			
+                </header>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/MyAccount.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/HomeRentState.css";
+
+
+const MyAccount = () => {    
+ 
+   
+    return (       
+        <div id="findProperty"> 
+
+            <div className="findProperty-container">
+                    Hola
+    
+
+              
+
+            </div>
+
+         
+        </div>      
+    );
+};
+
+export default MyAccount;
