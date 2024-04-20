043af29b4c0dfc0d9e55a9a898557cd45170038f
MichelleFMB <102604395+MichelleFMB@users.noreply.github.com>
2024-04-19 23:28:33 -0500

    feat - implementación de clientes interesados

---
+++ src/components/Clients.js
@@ -0,0 +1,36 @@
+import { Typography, Button } from '@mui/material';
+import '../styles/Clients.css'
+const Clients = () => {
+
+
+  return (
+    <div className="clients">
+      <Typography variant="h6" gutterBottom>
+        Clientes Interesados:
+      </Typography>
+
+      <div className="interestedClient">
+        <Typography>Pepita</Typography>
+        <div  className="interest_button">
+          <Button
+            variant="outlined"
+            color="primary"
+            className="accept_button"
+          >
+            Aceptar
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            
+          >
+            Rechazar
+          </Button>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Clients;
