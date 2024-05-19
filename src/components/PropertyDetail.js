9d22d232743c891eb4684aa9fb1a7ccea9363563
nirvanaGarciaV <84489764+nirvagarcia@users.noreply.github.com>
2024-05-19 01:15:28 -0500

    fixing property preview

--- src/components/PropertyDetail.js
+++ src/components/PropertyDetail.js
@@ -1,70 +1,60 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Card, CardHeader, CardContent, Typography, Button, Avatar, Grid } from '@mui/material';
-import MessageIcon from '@mui/icons-material/Message';
-import MapIcon from '@mui/icons-material/Map';
 import '../styles/PropertyDetail.css';
-import { properties } from '../auxiliars/MyConsts';
-import Comments from './Comments'; 
+import { properties, users } from '../auxiliars/MyConsts';
+import "../styles/MyAccount.css";
+import { Avatar } from '@mui/material';
 
-function PropertyDetail() {
+const PropertyDetail = () => {
+
     const { id } = useParams(); 
-    const propertyId = parseInt(id);
-
-
- 
+    const { userId } = useParams(); 
+    const propertyId = parseInt(id); 
     const selectedProperty = properties.find((property) => property.id === propertyId);
+
+    // const vinculatedOwner = users.find((owner) => owner.userId === parseInt(userId));
 
     if (!selectedProperty) {
         return <div>No se encontró la propiedad con el ID {propertyId}</div>;
     }
 
-    const { cardimage, category, district, address, details, price } = selectedProperty;
+    // if (!vinculatedOwner) {
+    //     return <div>No se encontró el dueño con el ID {parseInt(userId)}</div>;
+    // }
 
 
+    // const { name, lastName, email, photoUrl } = vinculatedOwner;
+
+    const { cardimage, district, location, characteristics, price } = selectedProperty;
 
     return (
-        <div className="cardDetails">
-            <Card>
-                <CardHeader title={category} subheader={`${district}, ${address}`} />
-                <CardContent>
-                    <Grid container spacing={2} className="grid_container">
-                        <Grid item xs={6}>
-                        <div className="avatarContainer">
+        <div className="myAccount-container" style={{margin:'7rem 0'}}>
+
+            <div style={{display:'flex', gap:'2rem', alignItems:'center' }}>
+                <img src={cardimage} alt="Property" style={{width:'60%',}} />
+                <div style={{display:'block', textAlign:'left'}}>
+                    <p style={{fontWeight:'bolder'}}>{district}</p>
+                    <p>{location}</p>                  
+                    <p>{characteristics}</p>
+                    <p>S/. {price}</p>
+
+                    <div style={{ display:'flex', gap:'1rem', marginTop:'3rem', alignItems:'center'}}>
+                        <div>
                             <Avatar alt="Avatar" src="/path-to-your-avatar.jpg" />
-                            <Typography variant="subtitle1" className="avatarName">
-                            John
-                            </Typography>
+                        </div> 
+                        <div>
+                            <div>Propietario: {district}</div>
+                            <div>{location}</div>  
                         </div>
-                        </Grid>
-                    </Grid>
+                    </div>  
+                </div>
 
-                    <img src={cardimage} alt="Property" className="propertyImage" />
-                    <Typography variant="body1" color="textSecondary" component="p">
-                        {details}
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                        $ {price}
-                    </Typography>
-                    <div className="cardActions">
-                        <Button variant="contained" color="primary" startIcon={<MessageIcon />} fullWidth>
-                        Enviar Mensaje
-                        </Button>
-                        <Button variant="outlined" color="primary" startIcon={<MapIcon />} fullWidth>
-                        Ver en Mapa
-                        </Button>
+              
+            </div>
 
-                      
-                    
-
-                   
-                   
-                    </div>
-                    <Comments />
-                </CardContent>
-            </Card>
+        
         </div>
     );
-    }
+};
 
 export default PropertyDetail;
