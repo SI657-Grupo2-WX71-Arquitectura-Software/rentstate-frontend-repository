5f09921514454b98fd4b4af58642eead08244878
MichelleFMB <102604395+MichelleFMB@users.noreply.github.com>
2024-06-07 01:50:34 -0500

    fix: modification property section

--- src/hooks/usePropertyService.js
+++ src/hooks/usePropertyService.js
@@ -1,48 +1,45 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8094'; // Cambia la URL base según la configuración de tu backend
+const BASE_URL = 'http://localhost:8094'; 
 
 const PropertyService = {
-  // Método para obtener todas las propiedades
   getAllProperties: async () => {
     try {
       const response = await axios.get(`${BASE_URL}/api/v1/properties`);
       return response.data; // Retorna los datos de todas las propiedades
     } catch (error) {
       console.error('Error al obtener todas las propiedades:', error);
-      throw error; // Manejar el error adecuadamente en tu componente React
+      throw error; 
     }
   },
 
-  // Método para obtener una propiedad por ID
   getPropertyById: async (propertyId) => {
     try {
       const response = await axios.get(`${BASE_URL}/api/v1/properties/${propertyId}`);
       return response.data; // Retorna los datos de la propiedad específica
     } catch (error) {
       console.error(`Error al obtener la propiedad con ID ${propertyId}:`, error);
-      throw error; // Manejar el error adecuadamente en tu componente React
+      throw error; 
     }
   },
 
-  // Método para obtener propiedades por userId
   getPropertiesByUserId: async (userId) => {
     try {
       const response = await axios.get(`${BASE_URL}/api/v1/properties?userId=${userId}`);
       return response.data; // Retorna los datos de las propiedades del usuario
     } catch (error) {
       console.error(`Error al obtener propiedades del usuario con ID ${userId}:`, error);
-      throw error; // Manejar el error adecuadamente en tu componente React
+      throw error; 
     }
   },
-   // Método para eliminar una propiedad por ID
-   deleteProperty: async (propertyId) => {
+
+  deleteProperty: async (propertyId) => {
     try {
       await axios.delete(`${BASE_URL}/api/v1/properties/${propertyId}`);
       // No necesitas devolver nada si el borrado es exitoso
     } catch (error) {
       console.error(`Error al eliminar la propiedad con ID ${propertyId}:`, error);
-      throw error; // Manejar el error adecuadamente en tu componente React
+      throw error; 
     }
   },
 
@@ -52,7 +49,7 @@ const PropertyService = {
       return response.data; // Retorna los datos de la propiedad creada
     } catch (error) {
       console.error('Error al crear la propiedad:', error);
-      throw error; // Manejar el error adecuadamente en tu componente React
+      throw error;
     }
   },
 };
