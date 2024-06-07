7028f79e24e15369c214ae76a3fb8c573dbff4e4
MichelleFMB <102604395+MichelleFMB@users.noreply.github.com>
2024-06-07 01:44:04 -0500

    feat: Post publish section

--- src/components/MyPublish.js
+++ src/components/MyPublish.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TextField, Button } from '@mui/material';
 import { toast, ToastContainer } from 'react-toastify'; // Importa react-toastify
-import 'react-toastify/dist/ReactToastify.css'; // Importa los estilos de react-toastify
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/Booking.css';
 import PostService from '../hooks/usePostService';
 
