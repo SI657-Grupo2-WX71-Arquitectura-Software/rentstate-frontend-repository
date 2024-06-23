353e3f1d55fe98473ebdff92daf5c0db974ebccc
MichelleFMB <102604395+MichelleFMB@users.noreply.github.com>
2024-06-22 20:25:19 -0500

    chore: elimination

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,7 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import { Typography, Button } from '@mui/material';
-import '../styles/Clients.css'
-const Clients = () => {
+import '../styles/Clients.css';
+import PropertyService from '../hooks/usePropertyService';
 
+const Clients = () => {
+  const [interestedClients, setInterestedClients] = useState([]);
+
+  useEffect(() => {
+    const fetchInterestedClients = async () => {
+      try {
+        // Realiza la solicitud para obtener todas las propiedades
+        const properties = await PropertyService.getAllProperties();
+        
+        // Filtra los nombres de los usuarios interesados
+        const clients = properties.reduce((clientsArray, property) => {
+          if (property.interest && property.interest.length > 0) {
+            property.interest.forEach(interest => {
+              if (!clientsArray.includes(interest.userName)) {
+                clientsArray.push(interest.userName);
+              }
+            });
+          }
+          return clientsArray;
+        }, []);
+
+        setInterestedClients(clients);
+      } catch (error) {
+        console.error('Error al obtener los clientes interesados:', error);
+      }
+    };
+
+    fetchInterestedClients();
+  }, []);
 
   return (
     <div className="clients">
@@ -9,25 +39,30 @@ const Clients = () => {
         Clientes Interesados:
       </Typography>
 
-      <div className="interestedClient">
-        <Typography>Pepita</Typography>
-        <div  className="interest_button">
-          <Button
-            variant="outlined"
-            color="primary"
-            className="accept_button"
-          >
-            Aceptar
-          </Button>
-          <Button
-            variant="outlined"
-            color="secondary"
-            
-          >
-            Rechazar
-          </Button>
-        </div>
-      </div>
+      {interestedClients.length > 0 ? (
+        interestedClients.map(clientName => (
+          <div key={clientName} className="interestedClient">
+            <Typography>{clientName}</Typography>
+            <div className="interest_button">
+              <Button
+                variant="outlined"
+                color="primary"
+                className="accept_button"
+              >
+                Aceptar
+              </Button>
+              <Button
+                variant="outlined"
+                color="secondary"
+              >
+                Rechazar
+              </Button>
+            </div>
+          </div>
+        ))
+      ) : (
+        <Typography>No hay clientes interesados.</Typography>
+      )}
 
     </div>
   );
